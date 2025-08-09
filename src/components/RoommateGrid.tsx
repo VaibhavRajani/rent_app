128b8007f3599ed308085f5a057c21ab14af158c
Vaibhav Rajani <vaibhav.rajanivit@gmail.com>
2025-08-09 03:15:56 -0400

    increase columns to 4 in large

--- src/components/RoommateGrid.tsx
+++ src/components/RoommateGrid.tsx
@@ -9,7 +9,7 @@ interface RoommateGridProps {
 
 export default function RoommateGrid({ roommates }: RoommateGridProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
       {roommates.map((roommate) => (
         <RoommateCard key={roommate.id} roommate={roommate} />
       ))}
